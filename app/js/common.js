0466f0d112e51948ce41dfa48cc3419a5f6641d6
Oleg <monetov@mail.ru>
2017-12-11 17:45:49 +0000

    NewJS

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,14 @@
 $(function() {
 
-	// Custom JS
+	var rotate = $('.rotate');
+	var inner_rotate = $('.inner-rotate');
+	
+	var btn = $('.btn');
+	
+	btn.click(function(){
+	    console.log(rotate);
+	    rotate.css({'opacity': 1, 'transform': 'rotate(270deg)'});
+	    inner_rotate.css({'opacity': 1, 'transform': 'rotate(360deg)'});
+	});
 
 });
